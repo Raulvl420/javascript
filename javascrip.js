108217a8b7997ca2e18232e6354f19fa2650dcc0
Raul Vera Lopez <veralopezr@gmail.com>
2023-12-02 21:19:00 +0100

    actualizando

--- javascrip.js
+++ javascrip.js
@@ -1,121 +1,189 @@
 
-/* function saludar() {
-    let nombre = prompt("Bienvenido cual es su nombre");
-    if (nombre == 5) {
-        alert(" Hola en que podemos ayudarte " + nombre);   
-    }   else {
-        alert(`${nombre} no es valido`)
-    }    
-}
-
-saludar(); // Si no escribe un nombre correcto no avanzar hacerlo con while */
-
-let nombre = prompt ("Bienvenido cual es tu nombre")
-
 let opcionUno = "Camaras de Seguridad"
 let opcionDos = "Redes"
 let opcionTres = "Control de Acceso"
 let opcionCuatro = "Servicio tecnico"
 
-function carritoPrecio(carrito){
-    let total = 0;
-    for (productos of carrito){
-        total += productos.precio;
-    }
-    return total;
+let carrito = [];
+const arrayA = []
+fetch("./camaras.json")
+  .then(response => response.json())
+  .then(camaras => {
+    arrayA.push(...camaras);
+    console.log(arrayA)
+                                                                                               // Tengo console log
+  })
+  .catch(error => {
+    console.error('Error al cargar productos:', error);
+  });
+
+const arrayB = []
+fetch("./redes.json")
+  .then(response => response.json())
+  .then(redes => {
+    arrayB.push(...redes);
+    console.log(arrayB)
+                                                                                                    //Tengo console log
+
+  })
+  .catch(error => {
+    console.error('Error al cargar productos:', error);
+  });
+
+let arrayC = []
+fetch("./acceso.json")
+  .then(response => response.json())
+  .then(acceso => {
+    arrayC.push(...acceso);
+    console.log(arrayC)
+                                                                                                  // Tengo console log
+  })
+  .catch(error => {
+    console.error('Error al cargar productos:', error);
+  });
+
+                                                                                                //Contenedor con los productos 
+function mostrarProducto (producto) {
+let contenedor = document.createElement("div");
+contenedor.innerHTML = ` <h3> Producto  :   ${producto.tipo}</h3>
+                         <p>  Modelo    :   ${producto.modelo}</p>
+                         <b> Precio     :   ${producto.precio}</b>`;
+
+let botonAgregar = document.createElement("button");
+botonAgregar.innerHTML = "Agregar a Carrito";
+botonAgregar.addEventListener("click", function() {
+agregarAlCarrito (producto);
+                                                                                                // Tengo que console log
+console.log("Agregado al carrito:", producto);
+});
+
+let botonEliminar = document.createElement("button");
+botonEliminar.innerHTML = "Eliminar del carrito";
+botonEliminar.addEventListener("click", function() {
+    eliminarDelCarrito (producto);
+                                                                                                // Tengo el console log
+console.log("Eliminado del carrito:", producto);
+});
+
+document.body.appendChild(contenedor);
+contenedor.classList.add("centradoDos");
+contenedor.appendChild(botonEliminar);
+contenedor.appendChild(botonAgregar);                         
 }
 
-class Producto{
-    constructor(tipo, modelo, precio){
-      this.tipo = tipo;
-      this.modelo = modelo;
-      this.precio = precio;
+let carritoDiv = document.createElement("div");
+carritoDiv.id = "carrito";
+carritoDiv.classList.add("carrito");
+document.body.appendChild(carritoDiv);
+
+function mostrarCarrito () {
+    let carritoDiv = document.getElementById("carrito");
+    if (carrito.length === 0){
+             carritoDiv.style.display = "none";  
+    }   else {
+              carritoDiv.style.display = "block"     
+              carritoDiv.innerHTML = "";
+              let totalCarrito = 0;
+        carrito.forEach(producto => {
+        let productoDiv = document.createElement("div");
+        productoDiv.innerHTML = `<h3>${producto.tipo}</h3>
+                                 <p>Modelo: ${producto.modelo}</p>
+                                 <b>Precio: ${producto.precio}</b> `;
+        totalCarrito += producto.precio;
+        carritoDiv.appendChild(productoDiv);
+    });
+
+    let totalDiv = document.createElement ("div");
+    totalDiv.classList.add("totalCarrito");
+    totalDiv.innerHTML = `<b> Total:${totalCarrito}</b>`;
+    carritoDiv.appendChild(totalDiv);
+    
+    let botonPago = document.createElement("button");
+    botonPago.innerHTML = "Pagar";
+    botonPago.addEventListener("click", function() {
+      alert ("pagaste")
+});
+    totalDiv.appendChild(botonPago)
+}
+                                                                                               // Agregar un Boton para el alert con el nombre de finalizo compra
+carritoDiv.classList.add("centradoUno")
+document.body.appendChild(carritoDiv);
+}
+
+function agregarAlCarrito(producto) {
+    carrito.push(producto);
+    mostrarCarrito();
+}
+
+function eliminarDelCarrito(producto) {
+    let index = carrito.indexOf(producto);
+    if (index !== -1) {
+        let productoEliminado = carrito.splice(index, 1)[0];
+    }
+    else {
+        console.log ("no hay nada ");
+    }
+    mostrarCarrito();
+}
+                                                                                              // Funcion SetTime
+async function mostrasProductoPagina (array) {
+        for (const producto of array) {
+            await new Promise(resolve => setTimeout(resolve,850));
+            mostrarProducto(producto);
+}
+}
+
+function limpiarContenido() {
+  let contenedores = document.getElementsByClassName("centradoDos");
+  for (let contenedor of contenedores) {
+      while (contenedor.firstChild) {
+          contenedor.removeChild(contenedor.firstChild);
+      }
   }
 }
-const producto1 = new Producto ("Camara IP", "SD10A248V-HNI-LAS", 5000)
-const producto2 = new Producto ("Camara IP", "SD10A248V-HNI-LAS", 5000)
-const producto3 = new Producto ("Camara IP", "SD10A248V-HNI-LAS", 5000)
 
-const producto4 = new Producto ("Switch", "CY-S824-10G", 1500)
-const producto5 = new Producto ("Switch", "CY-S824-10G", 1500) 
-const producto6 = new Producto ("Switch", "CY-S824-10G", 1500)
+                                                                                             // SessionStorage para el formulario
+function storage () {
+  let nombre = document.getElementById("nombre").value;
+  let correo = document.getElementById("correo").value;
+  let telefono = document.getElementById("telefono").value;
 
-const producto7 = new Producto ("Control de Acceso", "IMBIO460-PRO", 2000)
-const producto8 = new Producto ("Control de Acceso", "IMBIO460-PRO", 2000)
-const producto9 = new Producto ("Control de Acceso", "IMBIO460-PRO", 2000)
+  let datosSoporte = {
+    nombre: nombre,
+    correo: correo,
+    telefono: telefono,
+  };
+  
+  sessionStorage.setItem("datosSoporte",JSON.stringify(datosSoporte));
+  console.log("guardado con exito", datosSoporte);
+  }
 
-const arrayA = [producto1, producto2, producto3]
-const arrayB = [producto4, producto5, producto6]
-const arrayC = [ producto7, producto8, producto9]
+                                                                                              //Barra de navegacion
 
-function servicios() {
-    let opcion;
-    let soporte;
-    let carrito = [];
-do{
-    opcion = parseInt(prompt("En que podemos ayudarte?\n" + "1. Camaras de Seguridad\n" + "2. Redes\n" + "3. Control de Accesos\n" + "4. Servicio Tecnico\n" + "5. Finalizar compra\n" + "6. Salir\n" ));
+document.getElementById("camaras").addEventListener("click", function(event) {
+    limpiarContenido()
+    mostrasProductoPagina (arrayA);
+    mostrarCarrito(carrito);
+});
 
-switch (opcion){
-    case 1:
-        let modelosCamara = arrayA.map(producto =>`${producto.tipo}: ${producto.modelo}, Precio: $${producto.precio}`);
-        let seleccionCamara = prompt("Que modelo buscas:\n" + modelosCamara.join("\n")); // como ponerle un numero a cada producto del array?
-        if (seleccionCamara >= 1 && seleccionCamara <= arrayA.length){
-            carrito.push(arrayA[seleccionCamara -1]);
-            alert("Producto agregado")
-        }   else {
-                  alert("Ingrese un modelo valido")  
-        }
-        break;
-    case 2:
-        let modelosRedes = arrayB.map(producto =>`${producto.tipo}: ${producto.modelo}, Precio: $${producto.precio}`);
-        let seleccionRedes = prompt("Que modelo buscas:\n" + modelosRedes.join("\n"));
-        if (seleccionRedes >= 1 && seleccionRedes <= arrayA.length){
-            carrito.push(arrayB[seleccionRedes -1]);
-            alert("Producto agregado")
-        }   else {
-                  alert("Ingrese un modelo valido")  
-        }
-        break;
-    case 3:
-        let modelosAcceso = arrayC.map(producto =>`${producto.tipo}: ${producto.modelo}, Precio: $${producto.precio}`);
-        let seleccionAcceso = prompt("Que modelo buscas:\n" + modelosAcceso.join("\n"));
-        if (seleccionAcceso >= 1 && seleccionAcceso <= arrayA.length){
-            carrito.push(arrayC[seleccionAcceso -1]);
-            alert("Producto agregado")
-        }   else {
-                  alert("Ingrese un modelo valido")  
-        }
-        break;
-    case 4:
-        soporte = prompt("Necesita soporte tecnico?")
-        if(soporte === "si"){
-            const tiempoEstimado = new Date();
-            tiempoEstimado.setHours(tiempoEstimado.getHours() + 3);
-            let numeroTelefono = prompt("En Breve nos comunicaremos contigo "  + nombre + " indicanos tu numero telefonico")
-            alert(" Gracias por brindarnos su numero telefonico, te contactaremos el " + tiempoEstimado.toLocaleString() + ".") 
-            }
-            else{alert("Gracias por su visita")}
-        break;
-    case 5:
-        const totalCarrito = carritoPrecio(carrito);
-        if (carrito.length === 0) {
-        alert("El carrito está vacío.");
-    }   else {
-        alert("Sus productos son:\n" + carrito.map(producto => `${producto.tipo}: ${producto.modelo}, Precio: $${producto.precio}`).join("\n") + "\n\nTotal a pagar: $" + totalCarrito);
-    }
-    break;
-    case 6:
-        alert("Gracias por su visita")
-        break;        
-    default:
-        alert("opcion no valida")
-}   
-    } while (opcion !== 6);
-}
-servicios();
+document.getElementById("redes").addEventListener("click", function(event) {
+    limpiarContenido() 
+    mostrasProductoPagina (arrayB);
+    mostrarCarrito(carrito);
+});
 
-/* {} */
+document.getElementById("control").addEventListener("click", function(event) {
+    limpiarContenido()
+    mostrasProductoPagina (arrayC);
+    mostrarCarrito(carrito);
+});
 
-/* function opciones(){
-    alert(`Nuestros Servicios son ${opcionUno}, ${opcionDos}, ${opcionTres},${opcionCuatro}`);
-} */
+document.getElementById("soporte").addEventListener("click", function(event) {
+  limpiarContenido()
+  document.getElementById ("formulario").style.display = "block";
+  document.getElementById("enviar").addEventListener("click",function(event){
+    storage()
+    }) 
+});
+
+/* {} `  */
--- prueba.js
+++ prueba.js
@@ -1,30 +1,135 @@
-    // Generar un número aleatorio entre 1 y 100
-/*     
-    const random = Math.floor(Math.random() * 100) + 1;
-    let tries = 0;
+//Creación de las etiquetas
+//1 paso
+//creación de la etiqueta
+// let div = document.createElement("div");
 
-    function checkGuess() {
-      const respuestaUsuario = parseInt(document.getElementById("guessInput").value);
-      tries++;
+//2 paso
+//agregado de contenido
+// div.innerHTML = "Hola a todos";
 
-      if (guess === randomNumber) {
-        document.getElementById("message").textContent = `¡Correcto! ¡Adivinaste en ${tries} intentos!`;
-      } else if (guess < randomNumber) {
-        document.getElementById("message").textContent = "Más alto. Intenta de nuevo.";
-      } else {
-        document.getElementById("message").textContent = "Más bajo. Intenta de nuevo.";
-      }
+//3 paso
+//asignación de un padre
+//1 opcion en el body
+// document.body.append(div);
+
+//2 opción en un contenedor
+// let contenedor = document.getElementById("contenedor");
+// contenedor.push(div);
+
+// const productos = [
+//   { id: 1, nombre: "pantalon", precio: 1000 },
+//   { id: 2, nombre: "camisa", precio: 700 },
+// ];
+
+// localStorage.setItem("carrito", JSON.stringify(productos));
+
+// const agregarProducto = (nombre, precio) => {
+//   //traer datos del storage
+//   let productosJson = localStorage.getItem("carrito");
+//   let productos = JSON.parse(productosJson);
+
+//   //administrar el arreglo
+//   let producto = {
+//     id: productos.length + 1,
+//     nombre,
+//     precio,
+//   };
+
+//   productos.push(producto);
+
+//   //actualización del storage
+//   localStorage.setItem("carrito", JSON.stringify(productos));
+// };
+
+// let nombre = prompt("Ingrese el nombre del producto");
+// let precio = Number(prompt("Ingrese el precio del producto"));
+
+// agregarProducto(nombre, precio);
+
+//Crear una función para eliminar un producto por su id
+
+// const eliminarProducto = (id) => {
+//   let productosJson = localStorage.getItem("carrito");
+//   let productos = JSON.parse(productosJson);
+
+//   let productosFiltrados = productos.filter((item) => item.id != id);
+
+//   localStorage.setItem("carrito", JSON.stringify(productosFiltrados));
+// };
+
+// let id = Number(prompt("Ingrese el id del producto a eliminar"));
+// eliminarProducto(id);
+
+//una función que dado el id mmodifique el precio de un producto
+/* 
+const modificarPrecio = (id, precio) => {};
+
+function servicios() {
+    let opcion;
+    let soporte;
+    let carrito = [];
+do{
+    opcion = parseInt(prompt("En que podemos ayudarte?\n" + "1. Camaras de Seguridad\n" + "2. Redes\n" + "3. Control de Accesos\n" + "4. Servicio Tecnico\n" + "5. Finalizar compra\n" + "6. Salir\n" ));
+
+switch (opcion){
+    case 1:
+        let modelosCamara = arrayA.map(producto =>`${producto.tipo}: ${producto.modelo}, Precio: $${producto.precio}`);
+        let seleccionCamara = prompt("Que modelo buscas:\n" + modelosCamara.join("\n")); // como ponerle un numero a cada producto del array?
+        if (seleccionCamara >= 1 && seleccionCamara <= arrayA.length){
+            carrito.push(arrayA[seleccionCamara -1]);
+            alert("Producto agregado")
+        }   else {
+                  alert("Ingrese un modelo valido")  
+        }
+        break;
+    case 2:
+        let modelosRedes = arrayB.map(producto =>`${producto.tipo}: ${producto.modelo}, Precio: $${producto.precio}`);
+        let seleccionRedes = prompt("Que modelo buscas:\n" + modelosRedes.join("\n"));
+        if (seleccionRedes >= 1 && seleccionRedes <= arrayA.length){
+            carrito.push(arrayB[seleccionRedes -1]);
+            alert("Producto agregado")
+        }   else {
+                  alert("Ingrese un modelo valido")  
+        }
+        break;
+    case 3:
+        let modelosAcceso = arrayC.map(producto =>`${producto.tipo}: ${producto.modelo}, Precio: $${producto.precio}`);
+        let seleccionAcceso = prompt("Que modelo buscas:\n" + modelosAcceso.join("\n"));
+        if (seleccionAcceso >= 1 && seleccionAcceso <= arrayA.length){
+            carrito.push(arrayC[seleccionAcceso -1]);
+            alert("Producto agregado")
+        }   else {
+                  alert("Ingrese un modelo valido")  
+        }
+        break;
+    case 4:
+        soporte = prompt("Necesita soporte tecnico?")
+        if(soporte === "si"){
+            const tiempoEstimado = new Date();
+            tiempoEstimado.setHours(tiempoEstimado.getHours() + 3);
+            let numeroTelefono = prompt("En Breve nos comunicaremos contigo "  + nombre + " indicanos tu numero telefonico")
+            alert(" Gracias por brindarnos su numero telefonico, te contactaremos el " + tiempoEstimado.toLocaleString() + ".") 
+            }
+            else{alert("Gracias por su visita")}
+        break;
+    case 5:
+        const totalCarrito = carritoPrecio(carrito);
+        if (carrito.length === 0) {
+        alert("El carrito está vacío.");
+    }   else {
+        alert("Sus productos son:\n" + carrito.map(producto => `${producto.tipo}: ${producto.modelo}, Precio: $${producto.precio}`).join("\n") + "\n\nTotal a pagar: $" + totalCarrito);
     }
-  */
+    break;
+    case 6:
+        alert("Gracias por su visita")
+        break;        
+    default:
+        alert("opcion no valida")
+}   
+    } while (opcion !== 6);
 
-/* function saludar() {
-  let nombre = console.log ("Bienvenido cual es su nombre");
-  if (nombre == 5) {
-      alert(" Hola en que podemos ayudarte " + nombre);   
-  }   else {
-      alert(`${nombre} no es valido`)
-  }    
-}
-   */
+ */
 
-let nombre = console.log("hola raul")
+fetch("./productos.json")
+.then(response => response.json())
+.then((productos)=>console.log(productos));
